9356c75509b7a4ad5e22a87e069faa1263d3f819
JUST130 <90813748+JUST130@users.noreply.github.com>
2021-09-22 08:40:31 +0200

    Repo

---
+++ bable.config.js
@@ -0,0 +1,12 @@
+module.export ={
+	presents:[
+		[
+            '@bable/present-env',
+			{
+				targets:{
+					node: 'current',
+				},
+			},
+		],
+	],
+};
---
+++ index.js
@@ -0,0 +1,32 @@
+import fetch from 'node-fetch';
+//
+/** 
+*  Autor : Josepina Martin
+*  Software Engineering Intern
+* Deep Catch Namibia Holdings
+* Tecnical Assessment 
+/*
+
+const ENDPOINT = 'https://nt-cdn.s3.amazonaws.com/colors.json';
+
+
+/**
+ * @param  colour name
+ * @param  hex filter for code
+ * @param  comName filter for complementary colour name
+ * @param  CompHex filter for complememntary color Hex code
+ * @returns Promise
+*/
+
+const fetchColors = async ({name,hex,compname,CompHex}) =>{
+  const res = fetch(ENDPOINTS);
+ /*
+   var filterColors = color.filter (almond,pertwinkle,white,shadow,black (names)
+     return res[filter]=== fetchColors;
+  */console.log(response);
+
+return res;
+};
+
+//
+export default fetchColors;
